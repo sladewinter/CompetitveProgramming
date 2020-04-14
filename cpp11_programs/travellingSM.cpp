a96467c8d29f974f2d7e1cf4cee8352df243a7fb
sladewinter <sladewinter@gmail.com>
2020-04-14 06:46:10 +0530

    Integrated with Atom

--- cpp11_programs/travellingSM.cpp
+++ cpp11_programs/travellingSM.cpp
@@ -1,3 +1,4 @@
+// Basic Travelling Salesman Code
 #include <iostream>
 #include <cstring>
 using namespace std;
@@ -8,13 +9,13 @@ int memo[5][32];
 int tsp( int pos, int mask )
 {
     int &ans{ memo[pos][mask] };
-    
+
     if( ans != -1 )
         return ans;
 
     if( mask == 0 )
         ans = dist[pos][0];
-        
+
     else
     {
         ans = 99999;
@@ -26,7 +27,7 @@ int tsp( int pos, int mask )
             ans = min( ans, dist[pos][i] + tsp( i, mask ^ LSone ) );
             m -= LSone;
         }
-    }       
+    }
     return ans;
 }
 
@@ -38,9 +39,8 @@ int main()
         for( int i{0}; i < n; ++i )
             for( int j{0}; j < n; ++j )
                 scanf( "%d", &dist[i][j] );
-                
+
         printf( "%d\n", tsp( 0, ( 1 << (n - 1) ) - 1 ) );
     }
     return 0;
 }
-
