3a88f5540ef1dfdb8600807eb9df3eb979fc21fb
sladewinter <sladewinter@gmail.com>
2020-05-06 08:29:03 +0530

    Solving 0/1 Knapsack DP problem by modelling into DAG and applying Longest Path

---
+++ Ch 4 Graph/Special Graphs/uva-10130.cpp
@@ -0,0 +1,100 @@
+//Dynamic Programming problem modelled as Longest Path on DAG
+//UVa - 10130 - SuperSale - Classic 0/1 Knapsack Problem
+//Model states ( item_no, capacity_left) of DP as vertices of DAG
+
+#include <iostream>
+#include <cstdint>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <tuple>
+
+using namespace std;
+using ii = pair<int, int>;
+using vii = vector<ii>;
+using iii = tuple<int, int, int>;
+using viii = vector<iii>;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+
+//Note: Declaring large variables as local often ends stack memory
+int_least8_t P[1000], W[1000];       
+int N;
+vii topologic;
+vvb visited;
+viii AdjList[1001][31];
+
+void toposort( int x, int y )
+{
+    visited[x][y] = true;
+
+    int x1, y1, val;
+    for( auto &next : AdjList[x][y] )
+    {
+        tie( x1, y1, val ) = next;
+        if( !visited[x1][y1] )
+            toposort( x1, y1 ); 
+    }
+
+    topologic.push_back( { x, y } );
+}
+
+int main()
+{
+    int T, G, MW;
+    vvi dist;
+    scanf( "%d", &T );
+    while( T-- )
+    {
+        //Take input Profits and Weights
+        scanf( "%d", &N );
+        for( int i{0}; i < N; ++i )
+            scanf( "%d %d", &P[i], &W[i] );
+
+        //Let's fill up our Adjacency List in O(nW) time
+        for( int i{ 0 }; i < N; ++i )
+            for( int w{ 30 }; w >= 0; --w )
+            {
+                AdjList[i][w].clear();
+
+                //Not taking item i, 0 value added
+                AdjList[i][w].push_back({ i + 1, w, 0 });
+                
+                //Taking item i, P[i] value added
+                if( w - W[i] >= 0 )
+                  AdjList[i][w].push_back({ i + 1, w - W[i], P[i] });
+            }
+            
+
+        scanf( "%d", &G );
+
+        int ans{ 0 };
+        while( G-- )
+        {
+            scanf( "%d", &MW );
+
+            //Topologically sort our DAG
+            topologic.clear();
+            visited.assign( N + 1, vb( 30 + 1, false ) );
+            toposort( 0, MW );
+            reverse( topologic.begin(), topologic.end() );
+
+            int x, y, val;
+            dist.assign( N + 1, vi( MW + 1, 0 ) );
+            for( auto &u : topologic )
+                for( auto &v : AdjList[u.first][u.second] )
+                {
+                    tie( x, y, val ) = v;
+                    dist[x][y] = max( dist[x][y], 
+                        dist[u.first][u.second] + val );
+                }
+
+            ans += *max_element( dist[N].begin(), dist[N].end() );
+        }
+        printf( "%d\n", ans );
+    }
+    return 0;
+    
+}
