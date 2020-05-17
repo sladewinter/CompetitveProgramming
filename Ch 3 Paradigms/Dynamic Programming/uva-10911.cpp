25a9e6d693557fa65babe8d5523b7867f63e1897
sladewinter <sladewinter@gmail.com>
2020-05-17 08:09:02 +0530

    Top Down DP with BitMasking

--- Ch 3 Paradigms/Dynamic Programming/uva-10911.cpp
+++ Ch 3 Paradigms/Dynamic Programming/uva-10911.cpp
@@ -16,31 +16,29 @@ struct point
 	int x, y;
 };
 
-double dist( point stud1, point stud2 )
+double dist( point stud1, point stud2 )      //Euclidean distance
 {
 	return sqrt(pow(stud1.x-stud2.x, 2) + pow(stud1.y-stud2.y, 2));
 }
 
-int N;
-point studs[16];
-double memo[65536];
-double optimal( int mask )
+int N;                                   //No of students
+point studs[16];						//Store student coordinates
+double memo[65536];					   //DP Memoization table
+double optimal( int mask ) 
 {
 	double &ans{ memo[mask] };
 	if( ans != -1 )
 		return ans;
 
-	int stud1{ (int)log2( mask ) };
-	mask &= ~( 1 << stud1 );
+	int stud1{ (int)log2( mask ) };       //Leftmost set bit index
+	mask &= ~( 1 << stud1 );           
 
-	if( __builtin_popcount(mask) == 1 )
+	if( __builtin_popcount(mask) == 1 )   //No of set bits
 	{
 		int stud2{ (int)log2( mask ) };
 		return ( ans = dist( studs[stud1], studs[stud2] ) );
 	}
-
 	ans = INF;
-
 	int temp_mask{ mask };
 
 	while( temp_mask )
@@ -63,8 +61,8 @@ int main()
 			cin >> stud >> studs[i].x >> studs[i].y;
 
 		int mask{ (1 << 2 * N) - 1 };
-
 		fill( memo, memo + mask + 1, -1 );
+
 		printf( "Case %d: %.2lf\n", ++caseNo, optimal( mask ) );
 	}
 }
