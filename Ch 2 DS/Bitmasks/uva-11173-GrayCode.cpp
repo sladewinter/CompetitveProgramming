f438d0106aa4f6097dd1df9a1d8562e69c601228
sladewinter <sladewinter@gmail.com>
2020-05-19 09:08:29 +0530

    Find kth Gray code using simple XORing

---
+++ Ch 2 DS/Bitmasks/uva-11173-GrayCode.cpp
@@ -0,0 +1,16 @@
+//UVa - 11173 - Grey Codes
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int N, n, k;
+	scanf( "%d", &N );
+	while( N-- )
+	{
+		scanf( "%d %d", &n, &k );
+		printf( "%d\n", k ^ ( k >> 1 ) );
+	}
+	return 0;
+}													  
