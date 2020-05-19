beb159f3e59e090c49e3f300b4fb85caf8a9b951
sladewinter <sladewinter@gmail.com>
2020-05-19 08:17:39 +0530

    Demonstrate use of STL Linked List using an iterator

--- Ch 2 DS/Linked List/uva-11988.cpp
+++ Ch 2 DS/Linked List/uva-11988.cpp
@@ -2,32 +2,33 @@
 
 #include <iostream>
 #include <list>
-#include <cstring>
+#include <string>
 using namespace std;
 
 int main() 
 {
-	char line[10001];
+	string line;
 	list<char> ll;
-	list<char>::iterator iter;
 
-	while( scanf( "%s", line ) != EOF )
+	while( getline( cin, line ) )
 	{
 		ll.clear();
-		iter = ll.begin();
+		list<char>::iterator iter{ ll.begin() };
 
-		for( int i{0}; i < strlen(line); ++i )
-			switch( line[i] )
+		for( auto &ch : line )
+			switch( ch )
 			{
+				case '\r':
+				case '\n': break;
+				
 				case '[' : iter = ll.begin();
 						   break;
 
 				case ']' : iter = ll.end();
 						   break;
 
-				default  : ll.insert( iter, line[i] );
+				default  : ll.insert( iter, ch );
 			}
-
 		for( auto &ch : ll )
 			cout << ch;
 		cout << '\n';
