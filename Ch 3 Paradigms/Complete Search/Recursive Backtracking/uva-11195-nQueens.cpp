dd11cc5ea4e5b04198a9449a5df88b03ec9da55f
sladewinter <sladewinter@gmail.com>
2020-05-22 05:24:51 +0530

    Recursive Backtracking with Pruning using Fast Lookup with Bitmasks

--- Ch 3 Paradigms/Complete Search/Recursive Backtracking/uva-11195-nQueens.cpp
+++ Ch 3 Paradigms/Complete Search/Recursive Backtracking/uva-11195-nQueens.cpp
@@ -1,23 +1,27 @@
+//UVa 11195 - Another n-Queen Problem
+//Recursive Backtracking with Pruning
+//Using Fast Lookup with Bitmasks
+
 #include <iostream>
 #include <cstdint>
 
 using namespace std;
 
-std::int_fast16_t rowV{ 0 };
-std::int_fast32_t leftD{ 0 };
-std::int_fast32_t rightD{ 0 };
+std::int_fast16_t rowV{ 0 };         //Keeps track of empty rows
+std::int_fast32_t leftD{ 0 };       //Keeps track of empty left diagonals
+std::int_fast32_t rightD{ 0 };     //Keeps track of empty right diagonals
 
-char board[ 14 ][ 14 ];
+char board[ 14 ][ 14 ];          //n <= 14 sized board
 
 int n  { 0 };
 int ans{ 0 };
 
-void backtrack( int c )
+void backtrack( int c )     //Recursively visit columns
 {
-    if(c == n )
+    if(c == n )           //1 possible solution
         ++ans;
         
-    else
+    else               //Assign empty row checking collision using bitmask
     {
         for( int r{ 0 }; r < n; ++r )
             if(( board[r][c] != '*' ) && !( rowV   & ( 1 << r ))
@@ -49,7 +53,7 @@ int main()
         rightD = 0;
         ans    = 0;
         
-        backtrack( 0 );
+        backtrack( 0 );                      //Always start with column 0
         
         printf("Case %d: %d\n", ++lineNum, ans);
     }
